ae7ed1b2d331da8580c28b757c689b0b3b904b9f
obinox <parkjonggu1122@gmail.com>
2022-11-16 20:46:52 +0900

    edited

--- src/main/java/me/obinox/frontmove/handlers/FrontHandler.java
+++ src/main/java/me/obinox/frontmove/handlers/FrontHandler.java
@@ -18,28 +18,15 @@ public class FrontHandler implements Listener {
     @EventHandler
     public void onMoveFront(PlayerMoveEvent PME){
         Player p = PME.getPlayer();
-        if (p.getGameMode() == GameMode.CREATIVE){
-    //        World w = p.getWorld();
-//            Vector moving = PME.getFrom().clone().subtract(PME.getTo()).toVector().multiply(-1);
-//            Vector looking = p.getEyeLocation().getDirection();
-//            Vector diff = moving.clone().subtract(looking);
-//            Vector movingXZ = new Vector(moving.getX(), 0D, moving.getZ()).normalize();
-//            Vector lookingXZ = new Vector(looking.getX(), 0D, looking.getZ()).normalize();
-//            if (!Double.isNaN(diff.length()) && Math.toDegrees(movingXZ.clone().angle(lookingXZ)) < 65) {
-//                p.getLocation().setDirection(looking);
-//                PME.getTo().subtract(lookingXZ.clone().multiply(lookingXZ.dot(moving)));
-//            }
-            World w = p.getWorld();
-
+        if (p.getGameMode() == GameMode.SURVIVAL){
+            Location F = PME.getFrom();
+            Location T = PME.getTo();
+            Vector move = new Vector(F.getX()-T.getX(), 0D, F.getZ()-T.getZ()).normalize();
             Vector fact = p.getLocation().getDirection();
-            Vector move = new Vector(PME.getFrom().getX()-PME.getTo().getX(), 0D, PME.getFrom().getZ()-PME.getTo().getZ()).normalize();
             Vector factor = new Vector(fact.getX(), 0D, fact.getZ()).normalize();
-            Bukkit.getLogger().info(move+" "+factor);
-            Vector diff = factor.clone().subtract(move);
-            if (!Double.isNaN(diff.length()) && Math.toDegrees(move.clone().angle(factor)) < 65) {
-                p.getLocation().setDirection(fact);
-                Location to = new Location(w, PME.getFrom().getX(), PME.getTo().getY(), PME.getFrom().getZ(), PME.getTo().getYaw(), PME.getTo().getPitch());
-                PME.setTo(to);
+            Vector diff = move.clone().subtract(factor);
+            if (!Double.isNaN(diff.length()) && Math.toDegrees(move.clone().angle(factor)) > 115) {
+                PME.setTo(new Location(p.getWorld(), F.getX(), T.getY(), F.getZ(), T.getYaw(), T.getPitch()));
             }
         }
     }
