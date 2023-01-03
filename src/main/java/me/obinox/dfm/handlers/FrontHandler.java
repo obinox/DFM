22b935804e9f842b3627f4de89ae0345058059b4
obinox <parkjonggu1122@gmail.com>
2023-01-03 14:12:24 +0900

    edited

--- src/main/java/me/obinox/dfm/handlers/FrontHandler.java
+++ src/main/java/me/obinox/dfm/handlers/FrontHandler.java
@@ -9,22 +9,23 @@ import org.bukkit.event.Listener;
 import me.obinox.dfm.DFM;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
+import org.jetbrains.annotations.NotNull;
 
 public class FrontHandler implements Listener {
     public FrontHandler(DFM plugin) {
         Bukkit.getPluginManager().registerEvents(this, plugin);
     }
     @EventHandler
-    public void onMoveFront(PlayerMoveEvent PME){
+    public void onMoveFront(@NotNull PlayerMoveEvent PME){
         Player p = PME.getPlayer();
         if (p.getGameMode() == GameMode.SURVIVAL){
             Location F = PME.getFrom();
             Location T = PME.getTo();
             Vector move = new Vector(F.getX()-T.getX(), 0D, F.getZ()-T.getZ()).normalize();
-            Vector fact = p.getLocation().getDirection();
-            Vector factor = new Vector(fact.getX(), 0D, fact.getZ()).normalize();
-            Vector diff = move.clone().subtract(factor);
-            if (!Double.isNaN(diff.length()) && Math.toDegrees(move.clone().angle(factor)) > 115) {
+            Vector face = p.getLocation().getDirection();
+            Vector forward = new Vector(face.getX(), 0D, face.getZ()).normalize();
+            Vector diff = move.clone().subtract(forward);
+            if (!Double.isNaN(diff.length()) && Math.toDegrees(move.clone().angle(forward)) > 115) {
                 PME.setTo(new Location(p.getWorld(), F.getX(), T.getY(), F.getZ(), T.getYaw(), T.getPitch()));
             }
         }
